a51b78934ea865b6ac9abb98a12886f5c1014e76
juju-png <juliocesar71156@gmail.com>
2023-10-23 13:40:19 -0300

    atividade 04

---
+++ #atividade04.py
@@ -0,0 +1,8 @@
+# Escreva um numero e veja qual o seu antecessor e seu sucessor! 
+numero = float(input("digite um numero:"))
+antecessor = numero -1
+print(antecessor)
+sucessor = numero +1
+print(sucessor) 
+
+
